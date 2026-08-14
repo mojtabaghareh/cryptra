5e5e27a11d16b674130bee564d3c087981d54311
mojtabaghareh <Mojtabagharekeshi1379@gmail.com>
2026-08-14 15:41:04 +0330

    Delete packages/core/src/types directory

--- packages/core/src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './User';
-export * from './Wallet';
-export * from './Token';
-export * from './Trade';
-export * from './Swap';
-export * from './Order';
-export * from './Position';
