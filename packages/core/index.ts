8611a44e10ed6ce95df49da17a125547d57cbc6e
mojtabaghareh <Mojtabagharekeshi1379@gmail.com>
2026-08-14 20:37:19 +0330

    Update index.ts

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,1 +1,29 @@
+/**
+ * @cryptra/core
+ * Shared domain layer: Types, Constants, Errors, Validators.
+ * Every app/package/service in the Cryptra workspace should depend on this
+ * package instead of redefining these primitives locally.
+ */
+export * from './types/User';
+export * from './types/Wallet';
+export * from './types/Token';
+export * from './types/Trade';
+export * from './types/Swap';
+export * from './types/Order';
+export * from './types/Position';
+
+export * from './constants/chains';
+export * from './constants/tokens';
+export * from './constants/protocols';
+export * from './constants/fees';
+export * from './constants/levels';
+export * from './constants/languages';
+
+export * from './errors/ErrorCodes';
+export * from './errors/AppError';
+
+export * from './validators/userValidator';
+export * from './validators/walletValidator';
+export * from './validators/swapValidator';
+export * from './validators/tradeValidator';
 
