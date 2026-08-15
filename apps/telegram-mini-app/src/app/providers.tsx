5ccee2964249329ca0e0880e2789bdca04bffed5
mojtabaghareh <Mojtabagharekeshi1379@gmail.com>
2026-08-16 02:23:47 +0330

    Update providers.tsx

--- apps/telegram-mini-app/src/app/providers.tsx
+++ apps/telegram-mini-app/src/app/providers.tsx
@@ -1,1 +1,19 @@
+import React from 'react';
+import { I18nProvider, initI18n } from '@cryptra/i18n';
+import { ThemeProvider } from '../telegram/theme';
+import { TelegramProvider } from '../telegram/telegram';
+
+initI18n();
+
+export const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <I18nProvider>
+      <ThemeProvider>
+        <TelegramProvider>
+          {children}
+        </TelegramProvider>
+      </ThemeProvider>
+    </I18nProvider>
+  );
+};
 
