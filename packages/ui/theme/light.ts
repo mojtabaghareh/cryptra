f72a0a8d2a1f7f10122c72736b62dbd795e87d2b
mojtabaghareh <Mojtabagharekeshi1379@gmail.com>
2026-08-15 21:51:07 +0330

    Update light.ts

--- packages/ui/theme/light.ts
+++ packages/ui/theme/light.ts
@@ -1,1 +1,8 @@
+import { colors } from '../tokens/colors';
+
+export const lightTheme = colors.light;
+
+export const lightCSSVariables = Object.entries(colors.light)
+  .map(([key, value]) => `--cryptra-${key}: ${value};`)
+  .join('\n');
 
