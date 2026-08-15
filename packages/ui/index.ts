dceae4a8ca196c7698ed2795c48c3a113ddb499f
mojtabaghareh <Mojtabagharekeshi1379@gmail.com>
2026-08-15 21:58:12 +0330

    Create index.ts

---
+++ packages/ui/index.ts
@@ -0,0 +1,25 @@
+// Components
+export { Button, type ButtonProps } from './components/Button';
+export { Modal, type ModalProps } from './components/Modal';
+export { Input, type InputProps } from './components/Input';
+export { Spinner, type SpinnerProps } from './components/Spinner';
+
+// Icons
+export { WalletIcon, type WalletIconProps } from './icons/WalletIcon';
+export { TradeIcon, type TradeIconProps } from './icons/TradeIcon';
+export { SwapIcon, type SwapIconProps } from './icons/SwapIcon';
+
+// Theme
+export { darkTheme, darkCSSVariables } from './theme/dark';
+export { lightTheme, lightCSSVariables } from './theme/light';
+
+// Tokens
+export { colors, type ColorTheme } from './tokens/colors';
+export { spacing, type Spacing } from './tokens/spacing';
+export { typography, type Typography } from './tokens/typography';
+
+// Utils
+export { cn } from './utils/cn';
+
+// Styles
+import './styles.css';
