ea2c190df114bc3500bdc529f40f05bed8eb3272
mojtabaghareh <Mojtabagharekeshi1379@gmail.com>
2026-08-15 21:50:43 +0330

    Update dark.ts

--- packages/ui/theme/dark.ts
+++ packages/ui/theme/dark.ts
@@ -1,1 +1,8 @@
+import { colors } from '../tokens/colors';
+
+export const darkTheme = colors.dark;
+
+export const darkCSSVariables = Object.entries(colors.dark)
+  .map(([key, value]) => `--cryptra-${key}: ${value};`)
+  .join('\n');
 
