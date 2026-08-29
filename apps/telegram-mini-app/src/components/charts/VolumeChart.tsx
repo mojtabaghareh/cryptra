1037a3993e906b7ae44cbcdc8cf49ff202906850
mojtabaghareh <Mojtabagharekeshi1379@gmail.com>
2026-08-29 18:08:05 +0330

    feat(mini-app): VolumeChart

--- apps/telegram-mini-app/src/components/charts/VolumeChart.tsx
+++ apps/telegram-mini-app/src/components/charts/VolumeChart.tsx
@@ -1,1 +1,7 @@
+import { PriceChart } from './PriceChart';
 
+export function VolumeChart(props: { points?: number[] }) {
+  return <PriceChart label="Volume" points={props.points ?? [20, 35, 28, 50, 42, 60, 55]} />;
+}
+
+export default VolumeChart;
